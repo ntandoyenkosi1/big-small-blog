be5750ac5bf778ef3058829dae692af1334535bb
Ntandoyenkosi <nmendu@2u.com>
2023-09-13 00:34:14 +0200

    Added likes functionality for posts and comments

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,11 +1,21 @@
-function handleLike(event){
- let id=event.target.id
- //fetch for liking a comment using the id
+function handleLike(event) {
+    let id = event.target.id
+    //fetch for liking a comment using the id
+    var requestOptions = {
+        method: 'PUT'
+    };
+
+    fetch(`http://localhost:3001/api/posts/${id}/like`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            document.location.reload()
+        })
+        .catch(error => console.log('error', error));
 }
-function handleRedirect(event){
-    let id=event.target.id
-    console.log(id);
+function handleRedirect(event) {
+    let id = document.querySelector("input[type=button]").dataset.id
     //fetch for redirecting to the post page
-   }
-document.querySelectorAll("button").forEach((btn)=>btn.addEventListener("click",handleLike))
-document.querySelectorAll(".view").forEach((btn)=>btn.addEventListener("click",handleRedirect))
+    document.location.replace(`/posts/${id}`)
+}
+document.querySelectorAll("button").forEach((btn) => btn.addEventListener("click", handleLike))
+document.querySelectorAll(".view").forEach((btn) => btn.addEventListener("click", handleRedirect))
--- public/js/post.js
+++ public/js/post.js
@@ -1,5 +1,6 @@
 document.querySelector("#submit").addEventListener("click", function (e) {
     const comment = document.querySelector("input[type=text]").value
+    console.log(comment)
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -22,3 +23,18 @@ document.querySelector("#submit").addEventListener("click", function (e) {
         })
         .catch(error => console.log('error', error));
 })
+function handleLike(event) {
+    let id = event.target.id
+    //fetch for liking a comment using the id
+    var requestOptions = {
+        method: 'PUT'
+    };
+
+    fetch(`http://localhost:3001/api/comments/${id}/like`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            document.location.reload()
+        })
+        .catch(error => console.log('error', error));
+}
+document.querySelectorAll("button").forEach((btn) => btn.addEventListener("click", handleLike))
