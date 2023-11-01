2aa8a4d5528123ae8c9b4dd74402d6dbaad3da59
Ntandoyenkosi <nmendu@2u.com>
2023-11-01 23:49:03 +0200

    Added a Dockerfile for containerization Modified seeds to sync models Modified post model

--- config/index.js
+++ config/index.js
@@ -1,7 +1,12 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-
+/**
+ * Uncomment the code below in order to use SQLite.
+ * SQLite comes in handy in deployment environments aimed at utilizing less resources
+ * @install SQLite using `npm install sqlite3`
+ */
+//const sequelize= new Sequelize({dialect:"sqlite", storage:"blog.db"})
 const sequelize = new Sequelize( process.env.DB_NAMES, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST,
       dialect: 'mysql',
--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,11 @@
-const userData=require("./users")
-const postData=require("./posts")
-const commentData=require("./comments")
-const {User,Post,Comment}=require("../models")
+const userData = require("./users")
+const postData = require("./posts")
+const commentData = require("./comments")
+const { User, Post, Comment } = require("../models")
+const sequelize = require("../config")
 
+// forces sequelize to sync models with db before seeding data
+sequelize.sync({ force: true }).then(() => {
   User.bulkCreate(userData)
     .then(() => Post.bulkCreate(postData))
     .then(() => Comment.bulkCreate(commentData))
@@ -12,4 +15,5 @@ const {User,Post,Comment}=require("../models")
     .catch((err) => {
       console.error('Error inserting seed data:', err);
     });
-  
+})
+
