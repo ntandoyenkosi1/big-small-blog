4bd26ee07e97661cbee7752ec90e2fc45f26cc43
sntan <s.ntando99@gmail.com>
2024-07-05 00:21:06 +0200

    Added a profile page and made fields required on sign up

--- config/index.js
+++ config/index.js
@@ -1,16 +1,21 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 /**
  * Uncomment the code below in order to use SQLite.
  * SQLite comes in handy in deployment environments aimed at utilizing less resources
  * @install SQLite using `npm install sqlite3`
  */
-//const sequelize= new Sequelize({dialect:"sqlite", storage:"blog.db"})
-const sequelize = new Sequelize( process.env.DB_NAMES, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306
-    });
+const sequelize = new Sequelize({ dialect: "sqlite", storage: "blog.db" });
+// const sequelize = new Sequelize(
+//   process.env.DB_NAMES,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
--- controllers/home.routes.js
+++ controllers/home.routes.js
@@ -1,55 +1,70 @@
-const { Post, Comment, User } = require("../models")
-const auth = require("../middleware/auth")
-const router = require("express").Router()
+const { Post, Comment, User } = require("../models");
+const auth = require("../middleware/auth");
+const router = require("express").Router();
 
-router.get("/", async(req,res)=>{
-    res.render("homepage")
-})
+router.get("/", async (req, res) => {
+  res.render("homepage");
+});
 router.get("/posts", async (req, res) => {
-    let posts = await Post.findAll({ include: [{ model: Comment },{model:User}] })
-    posts = posts.map(x => x.get({ plain: true }))
-    posts?.map((x=>{
-        x.loggedIn=req.session.loggedIn
-        x.commentsCount=x.Comments.length
-        return x
-    }))
-    res.render("posts", { posts, loggedIn: req.session.loggedIn })
-})
+  let posts = await Post.findAll({
+    include: [{ model: Comment }, { model: User }],
+  });
+  posts = posts.map((x) => x.get({ plain: true }));
+  posts?.map((x) => {
+    x.loggedIn = req.session.loggedIn;
+    x.commentsCount = x.Comments.length;
+    return x;
+  });
+  res.render("posts", { posts, loggedIn: req.session.loggedIn });
+});
 router.get("/posts/create", (req, res) => {
-    res.render("create", { loggedIn: req.session.loggedIn })
-})
+  res.render("create", { loggedIn: req.session.loggedIn });
+});
 router.get("/posts/:id", async (req, res) => {
-    
-    let post = await Post.findByPk(req.params.id, { include: [{ model: Comment, include:User }, {model:User}] })
-    post = post.get({ plain: true })
-    post.Comments?.map((x=>x.loggedIn=req.session.loggedIn))
-    res.render("post", { post, loggedIn: req.session.loggedIn })
-})
+  let post = await Post.findByPk(req.params.id, {
+    include: [{ model: Comment, include: User }, { model: User }],
+  });
+  post = post.get({ plain: true });
+  post.Comments?.map((x) => (x.loggedIn = req.session.loggedIn));
+  res.render("post", { post, loggedIn: req.session.loggedIn });
+});
 router.get("/posts/edit/:id", auth, async (req, res) => {
-    let post = await Post.findByPk(req.params.id)
-    post = post.get({ plain: true })
-    res.render("edit", { post, loggedIn: req.session.loggedIn })
-})
+  let post = await Post.findByPk(req.params.id);
+  post = post.get({ plain: true });
+  res.render("edit", { post, loggedIn: req.session.loggedIn });
+});
 router.get("/posts/delete/:id", auth, (req, res) => {
-    res.render("delete", { loggedIn: req.session.loggedIn })
-})
+  res.render("delete", { loggedIn: req.session.loggedIn });
+});
 
 router.get("/dashboard", auth, async (req, res) => {
-    if (!req.session.user) return res.redirect("/login")
-    let posts = await Post.findAll({ where: { UserId: req.session.user.id } }, { include: { model: Comment } })
-    posts = posts.map(x => x.get({ plain: true }))
-    res.render("dashboard", { posts, loggedIn: req.session.loggedIn })
-})
+  if (!req.session.user) return res.redirect("/login");
+  let posts = await Post.findAll(
+    { where: { UserId: req.session.user.id } },
+    { include: { model: Comment } }
+  );
+  posts = posts.map((x) => x.get({ plain: true }));
+  res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
+});
 router.get("/login", async (req, res) => {
-    if (req.session.loggedIn) return res.redirect("/dashboard")
-    res.render("login")
-})
+  if (req.session.loggedIn) return res.redirect("/dashboard");
+  res.render("login");
+});
 router.get("/register", async (req, res) => {
-    res.render("signup")
-})
+  res.render("signup");
+});
 router.get("/logout", async (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/")
-    })
-})
-module.exports = router
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+router.get("/users/:id", async (req, res) => {
+  if (req.session.loggedIn) {
+    let user = await User.findByPk(req.params.id);
+    user = user.get({ plain: true });
+    res.render("user", { user });
+  } else {
+    res.redirect("/");
+  }
+});
+module.exports = router;
